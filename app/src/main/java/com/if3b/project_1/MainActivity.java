b768e5b76a65483b04fa5e61d628466c190f82aa
farizz <fairizwalker@gmail.com>
2022-10-12 15:00:14 +0700

    step 1

---
+++ app/src/main/java/com/if3b/project_1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.if3b.project_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etNpm, etNama;
+    private Button btnDaftar;
+    private RadioGroup rgJK;
+    private int selectedId;
+    private RadioButton rdJK;
+    private Spinner spProdi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etNpm = findViewById(R.id)
+    }
+}
